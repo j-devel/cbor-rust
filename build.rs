83bafbb48af6501b635e717ae20cfe6394fdd8b4
j-devel <j@w3reality.com>
2022-03-29 11:07:35 +0700

    Remove minerva_refract

--- build.rs
+++
@@ -1,9 +0,0 @@
-extern crate minerva_refract;
-use minerva_refract::expose_under_target;
-
-fn main() {
-    println!("cargo:rerun-if-changed=src/test_serializer.rs");
-    println!("cargo:rerun-if-changed=src/test_decoder.rs");
-    expose_under_target("src/test_serializer.rs", Some("refract_moz_cbor"), "test_serializer.rs").unwrap();
-    expose_under_target("src/test_decoder.rs", Some("refract_moz_cbor"), "test_decoder.rs").unwrap();
-}
